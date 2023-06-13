06c309e1339a2938104d3cecca33c09b6b983b57
Jonatan Padilla <jonatan.padilla@soytul.com>
2023-06-13 12:50:10 -0500

    Initial Commit

---
+++ pokemonApp/pokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonAppApp.swift
+//  pokemonApp
+//
+//  Created by Jonatan Padilla on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct pokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
